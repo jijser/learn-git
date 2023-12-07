6736341a2cf1d5da5583b6a5378ecd2756c5b650
jijser <109586932+jijser@users.noreply.github.com>
2023-12-07 13:48:09 +0700

    test Img

--- protfolio-chanathip/src/App.js
+++ protfolio-chanathip/src/App.js
@@ -6,9 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <p>
-          Chanathip Suksri (OAT)
-        </p>
         <img src={image} />
       </header>
     </div>
